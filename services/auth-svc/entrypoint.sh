b9dda0d36c1bacf55132cb7e5932aa57c2ed32de
Olzhas <ahm.olzhas3332@gmail.com>
2025-06-29 00:07:25 +0500

    Refactor Docker setup: standardize container names and update entrypoint script paths

--- services/auth-svc/entrypoint.sh
+++ services/auth-svc/entrypoint.sh
@@ -5,7 +5,7 @@ timeout 30s sh -c "until pg_isready -h '$AUTH_DB_HOST' -p '$AUTH_DB_PORT' -U '$A
 
 DSN="user=$AUTH_DB_USER password=$AUTH_DB_PASSWORD host=$AUTH_DB_HOST port=$AUTH_DB_PORT dbname=$AUTH_DB_NAME sslmode=disable"
 
-echo "Applying migrations from: $(ls -d /db/migrations)"
-/usr/local/bin/goose -dir /db/migrations postgres "$DSN" up
+echo "Applying migrations from: $(ls -d db/migrations)"
+/usr/local/bin/goose -dir db/migrations postgres "$DSN" up
 
-exec /usr/local/bin/auth "$@"
+exec ./auth "$@"
